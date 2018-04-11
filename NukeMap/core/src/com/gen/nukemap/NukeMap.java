b17422f6e9b9c1975bc3d748d2146335c8a05947
zedsdead <mr.walid.koubaa@gmail.com>
2018-04-11 16:53:27 +0200

    Sounds added

--- NukeMap/core/src/com/gen/nukemap/NukeMap.java
+++ NukeMap/core/src/com/gen/nukemap/NukeMap.java
@@ -2,6 +2,8 @@ package com.gen.nukemap;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,12 +11,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class NukeMap extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	public static AssetManager manager;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-	}
+		manager.load("assets/music/Jihad Trap - Drop The Bomb.mp3", Music.class);
+        manager.load("assets/sound/explosion.wav", Sound.class);
+        manager.load("assets/sound/explosion.wav2", Sound.class);
+        manager.finishLoading();
+
+
+    }
 
 	@Override
 	public void render () {
